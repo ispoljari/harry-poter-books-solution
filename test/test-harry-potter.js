cda0455f926df37443e7737c148bada17124c7dc
ispoljari <ivanspoljaric22@gmail.com>
2019-08-13 13:23:58 +0200

    Add test cases for simple discount combinations

--- test/test-harry-potter.js
+++ test/test-harry-potter.js
@@ -6,6 +6,8 @@ const { calcLowestPrice, basePrice, discounts } = import('./main.js'); //TODO:
 //   return faker.random.number(min = minValue, max = maxValue);
 // }
 
+
+
 describe('Harry Potter books', function () {
   it('Basic tests with total zero or 1 recuring unique book', function () {
     const basicCases = [
@@ -37,7 +39,29 @@ describe('Harry Potter books', function () {
     });
   });
 
-  it('Simple discounts', function () {
-    // some code goes here
+  it('Basic test with simple discount combinations', function () {
+    const basicCases = [
+      {
+        dummyShoppingCart: [1, 1, 0, 0, 0],
+        expected: 2 * basePrice * discounts.twoUniqueBooks,
+      },
+      {
+        dummyShoppingCart: [1, 1, 1, 0, 0],
+        expected: 3 * basePrice * discounts.threeUniqueBooks,
+      },
+      {
+        dummyShoppingCart: [1, 1, 1, 1, 0],
+        expected: 4 * basePrice * discounts.fourUniqueBooks,
+      },
+      {
+        dummyShoppingCart: [1, 1, 1, 1, 1],
+        expected: 5 * basePrice * discounts.fiveUniqueBooks,
+      },
+    ];
+
+    basicCases.forEach(input => {
+      const totalPrice = calcLowestPrice(input.dummyShoppingCart);
+      expect(totalPrice).to.equal(input.expected);
+    })
   });
 });
