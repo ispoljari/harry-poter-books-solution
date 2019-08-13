be2bca743b3ef47a99235e49ff708f512bf2118d
ispoljari <ivanspoljaric22@gmail.com>
2019-08-13 12:15:45 +0200

    Add basic test scaffolding code

---
+++ test/test-harry-potter.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+
+describe('Harry Potter books', function () {
+  it('Basic tests', function () {
+    // some code goes here
+  });
+
+  it('Simple discounts', function () {
+    // some code goes here
+  });
+});
