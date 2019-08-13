62d2c49fdc0208e63d7bf7aa7a17003e6213934d
ispoljari <ivanspoljaric22@gmail.com>
2019-08-13 13:16:19 +0200

    Create initial test suite for basic tests without discount

--- test/test-harry-potter.js
+++ test/test-harry-potter.js
@@ -1,8 +1,40 @@
 const { expect } = require('chai');
+const faker = import('faker');
+const { calcLowestPrice, basePrice, discounts } = import('./main.js'); //TODO:
+
+// const generateRandNumber = (minValue, maxValue) => {
+//   return faker.random.number(min = minValue, max = maxValue);
+// }
 
 describe('Harry Potter books', function () {
-  it('Basic tests', function () {
-    // some code goes here
+  it('Basic tests with total zero or 1 recuring unique book', function () {
+    const basicCases = [
+      {
+        dummyShoppingCart: [0, 0, 0, 0, 0],
+        expected: 0,
+      },
+      {
+        dummyShoppingCart: [1, 0, 0, 0, 0],
+        expected: basePrice,
+      },
+      {
+        dummyShoppingCart: [0, 0, 0, 0, 1],
+        expected: basePrice,
+      },
+      {
+        dummyShoppingCart: [0, 7, 0, 0, 0],
+        expected: 7 * basePrice,
+      },
+      {
+        dummyShoppingCart: [0, 0, 6, 0, 0],
+        expected: 6 * basePrice,
+      },
+    ];
+
+    basicCases.forEach(input => {
+      const totalPrice = calcLowestPrice(input.dummyShoppingCart);
+      expect(totalPrice).to.equal(input.expected);
+    });
   });
 
   it('Simple discounts', function () {
